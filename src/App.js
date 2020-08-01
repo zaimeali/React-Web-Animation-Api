1a9d01c81f70aec7151f835704fe086c97499a90
abdul-raffay <abdul.raffay@nytrotech.com>
2020-08-01 19:07:27 +0500

    function left]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 // Web Animation API
@@ -11,46 +11,104 @@ import Wheel from './img/wheel.png';
 
 function App() {
 
-  const ref = useRef();
-
-  const { ref: roadStrip1, getAnimation: road1Animation } = useWebAnimations({
+  const { ref: highway, getAnimation: highwayAnimation } = useWebAnimations({
     keyframes: [
-      { transform: 'translateX(0)' },
-      { transform: 'translateX(-2500px)' }
+      { transform: 'translateX(-3400px)', },
     ],
     timing: {
-      duration: 12000,
+      duration: 5000,
       iterations: Infinity,
-      direction: "normal"
     }
   });
+
+  const { ref: city, getAnimation: cityAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(-1400px)', },
+    ],
+    timing: {
+      duration: 20000,
+      iterations: Infinity,
+    }
+  });
+  
+  const { ref: car, getAnimation: carAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(-1px)', },
+      { transform: 'translateX(2px)', },
+      { transform: 'translateX(-1px)', },
+    ],
+    timing: {
+      duration: 1000,
+      iterations: Infinity,
+    }
+  });
+
+  const { ref: wheel1, getAnimation: wheel1Animation } = useWebAnimations({
+    keyframes: [
+      { transform: 'rotate(360deg)', },
+    ],
+    timing: {
+      duration: 1000,
+      iterations: Infinity,
+    }
+  });
+  
+  const { ref: wheel2, getAnimation: wheel2Animation } = useWebAnimations({
+    keyframes: [
+      { transform: 'rotate(360deg)', },
+    ],
+    timing: {
+      duration: 1000,
+      iterations: Infinity,
+    }
+  });
+
+  function start() {
+
+  }
+
+  function stop() {
+
+  }
+
+  function increase() {
+
+  }
+
+  function decrease() {
+
+  }
+
+  useEffect(() => {
+    highwayAnimation().pause();
+    cityAnimation().pause();
+    carAnimation().pause();
+    wheel1Animation().pause();
+    wheel2Animation().pause();
+  }, [highwayAnimation, cityAnimation, carAnimation, wheel1Animation, wheel2Animation])
 
   return (
     <div className="App">
 
-      {/* <div className="road">
-        <div ref={ roadStrip1 } className="roadStrip1"></div>
-      </div> */}
-
       <div className="hero">
-        <div className="highway" id="highway" />
-        <div className="city" id="city" />
+        <div ref={ highway } className="highway" id="highway" />
+        <div ref={ city } className="city" id="city" />
 
         <div className="car">
-          <img id="car" src={ Car } alt="car" />
+          <img ref={ car } id="car" src={ Car } alt="car" />
         </div>
 
         <div className="wheel">
-          <img id="img1" src={ Wheel } className="back-wheel" alt="wheel" />
-          <img id="img2" src={ Wheel } className="front-wheel" alt="wheel" />
+          <img ref={ wheel1 } id="img1" src={ Wheel } className="back-wheel" alt="wheel" />
+          <img ref={ wheel2 } id="img2" src={ Wheel } className="front-wheel" alt="wheel" />
         </div>
       </div>
 
       
-      <button onClick="start()" class="btn" id="start">Start</button>
-      <button disabled onClick="stop()" class="btn" id="stop">Stop</button>
-      <button disabled onClick="increase()" class="btn" id="increase">Increase</button>
-      <button disabled onClick="decrease()" class="btn" id="decrease">Decrease</button>
+      <button onClick={ start } className="btn" id="start">Start</button>
+      <button disabled onClick={ stop } className="btn" id="stop">Stop</button>
+      <button disabled onClick={ increase } className="btn" id="increase">Increase</button>
+      <button disabled onClick={ decrease } className="btn" id="decrease">Decrease</button>
 
     </div>
   );
